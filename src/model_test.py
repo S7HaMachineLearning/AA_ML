375123470a988f0b4a45756d701f09fbab862f67
Bastiaan C <131658@student.fontys.nl>
2023-06-11 18:38:49 +0200

    minor fixes

--- src/model_test.py
+++ src/model_test.py
@@ -1,19 +1,19 @@
+"""Test the model module."""
 from fastapi.testclient import TestClient
-import pytest
 import main  # import the main module
 
 # I've created a mock MachineLearning class that doesn't load any model or tokenizer and always
 # returns "Mock automation" when the generate_automation method is called.
-# I've replaced the MachineLearning class in the main module with this mock class for the duration of the test.
-# I've defined the request body, made a request to the API, and checked that the response status code is 200
+# I've replaced the MachineLearning class in the main module with this mock class for the duration of the test.  # pylint: disable=line-too-long
+# I've defined the request body, made a request to the API, and checked that the response status code is 200  # pylint: disable=line-too-long
 # and the response body contains the expected result.
 
 client = TestClient(main.app)  # use the app from the main module
 
 
 def test_generate_automation():
-    # Mock the MachineLearning class
-    class MockMachineLearning(main.MachineLearning):  # use the MachineLearning class from the main module  # pylint: line-too-long
+    """# Mock the MachineLearning class"""
+    class MockMachineLearning(main.MachineLearning):
         def load_model(self, model_path, tokenizer_path):
             pass
 
@@ -25,7 +25,7 @@ def test_generate_automation():
 
     # Define the request body
     request_body = {
-        "start_sequence": "{\"alias\": \"Example automation\", \"trigger\": {\"platform\": \"state\", \"entity_id\": \"sun.sun\", \"to\": \"below_horizon\"}, \"condition\": {\"condition\": \"state\", \"entity_id\": \"device_tracker.person1\", \"state\": \"home\"}, \"action\": {\"service\": \"light.turn_on\", \"target\": {\"entity_id\": \"light.living_room\"}}"  # pylint: line-too-long
+        "start_sequence": "{\"alias\": \"Example automation\", \"trigger\": {\"platform\": \"state\", \"entity_id\": \"sun.sun\", \"to\": \"below_horizon\"}, \"condition\": {\"condition\": \"state\", \"entity_id\": \"device_tracker.person1\", \"state\": \"home\"}, \"action\": {\"service\": \"light.turn_on\", \"target\": {\"entity_id\": \"light.living_room\"}}"  # pylint: disable=line-too-long
     }
 
     # Make a request to the API
